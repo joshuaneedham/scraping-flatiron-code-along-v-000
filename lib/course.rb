0c158f8528ea3e0a33d12ae23f0412f4416378f7
Joshua Needham <joshuabneedham@gmail.com>
2017-11-28 04:04:52 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,13 +2,6 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-  def initialize(title)
-    @title = title
-    @schedule = schedule
-    @description = description
-    @@all
-  end
-
   def self.all
     @@all
   end
