d9ee74cb24bd8f701f0a36c559dad015867f3707
Joshua Needham <joshuabneedham@gmail.com>
2017-11-28 04:41:32 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,8 +16,21 @@ class Scraper
     #   course.description = post.css("p").text
     # end
   end
+
+  def get_courses
+    self.get_page_css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+      end
+    end
+      
 end
-Scraper.new.get_page
     # def print_courses
     #   self.make_courses
     #   Course.all.each do |course|
